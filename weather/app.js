d926230e6b712809c5eb6d03c63da983a4ae1a9e
Andial <luisandiamamani@gmail.com>
2019-02-14 00:41:06 -0500

    lat,lng url

--- weather/app.js
+++ weather/app.js
@@ -1,5 +1,21 @@
+const axios = require('axios');
 const argv = require('./config/yargs').argv;
 const fs = require('fs');
-let comando = argv._[0];
+let comando = argv.direccion;
+
+let encoderUrl = encodeURI(comando)
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encoderUrl}&key=AIzaSyAQrdi99J4kzVQNRc0PtDRvX4dLyOkmde0`)
+  .then(resp => {
+    let location  = resp.data.results[0]
+    let coords = location.geometry.location;
+    console.log(coords.lat)
+    console.log(coords.lng)
+    console.log(location.formatted_address);
+    // console.log(JSON.stringify(resp.data,undefined,2) );
+  }).catch(e => {
+    console.log("Error", e);
+  });
+
 
 
--- weather/config/yargs.js
+++ weather/config/yargs.js
@@ -12,39 +12,6 @@ const argv = require('yargs').command('create', '--> create task', (yargs) => {
   return yargs.options({
     description
   });
-}).command('update', '--> update task', (yargs) => {
-  return yargs.options({
-    description: {
-      alias: 'd',
-      type: 'string',
-      demand: 'Please specify string',
-      nargs: 1,
-      string: true,
-      required: true,
-      desc: 'string to update',
-    },
-    compleado: {
-      alias: 'c',
-      type: 'string',
-      demand: 'Please specify string',
-      nargs: 1,
-      string: true,
-      required: true,
-      desc: 'mark as complete a task',
-    }
-  });
-}).command('erase', '--> erase task', (yargs) => {
-  return yargs.options({
-    description: {
-      alias: 'd',
-      type: 'string',
-      demand: 'Please specify string',
-      nargs: 1,
-      string: true,
-      required: true,
-      desc: 'string to erase',
-    }
-  });
 })
   .help().argv;
 
