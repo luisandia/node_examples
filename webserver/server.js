3db79083af952c35b93c8fa7cc756627c0e9530d
Andial <luisandiamamani@gmail.com>
2019-02-17 00:26:27 -0500

    finish webserver

--- webserver/server.js
+++ webserver/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000
+// hbs.registerHelper('helper_name', function(...) { ... });
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(__dirname + '/public'));
 
 //express hbs
 
 app.set('view engine', 'hbs');
+
+// helpers
+
+
+
 app.get('/', function (req, res) {
   res.render('home', {
     name: 'luis',
     anio: new Date().getFullYear()
   });
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.get('/about', function (req, res) {
+  res.render('about', {
+    name: 'luis',
+    anio: new Date().getFullYear()
+  });
+})
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
