2108d570389a52883396f41ac91b03fb04ce720c
Andial <luisandiamamani@gmail.com>
2019-02-21 23:56:08 -0500

    Advance rest_resver

--- rest_server/server/models/usuario.js
+++ rest_server/server/models/usuario.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
-
+const uniqueValidator = require('mongoose-unique-validator');
 let Schema = mongoose.Schema;
+
+let rolesValidos= {
+  values:['ADMIN_ROLE','USER_ROLE'],
+  message:'{VALUE} no es un rol valido'
+}
 
 let usuarioSchema = new Schema({
   nombre: {
@@ -22,7 +27,8 @@ let usuarioSchema = new Schema({
   },
   role: {
     type:String,
-    default: 'USER_ROLE'
+    default: 'USER_ROLE',
+    enum:rolesValidos
   },
   estado: {
     type: Boolean,
@@ -34,5 +40,6 @@ let usuarioSchema = new Schema({
   }
 });
 
+usuarioSchema.plugin(uniqueValidator,{message:'{PATH} debe de ser unico'});
 module.exports = mongoose.model('Usuario',usuarioSchema);
 
