8a115fc37ad3c57ec14347a42786bf6260ff535a
Andial <luisandiamamani@gmail.com>
2019-03-02 23:59:00 -0500

    cluster

---
+++ node_intermediate/index.js
@@ -0,0 +1,26 @@
+process.env.UV_THREADPOOL_SIZE=1;
+const cluster = require('cluster');
+const crypto = require('crypto');
+if (cluster.isMaster) {
+  //index.js executed again but in slave mode
+  cluster.fork();
+  cluster.fork();
+} else {
+  //childs, act like a server
+  const express = require('express');
+  const app = express();
+
+  app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
+      res.send('Hello World!');
+      
+    });
+  });
+  app.get('/fast', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
+}
