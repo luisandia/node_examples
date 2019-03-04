df7bdb19d3f2263f5f63118e05e87c0723f10d0c
Andial <luisandiamamani@gmail.com>
2019-03-03 21:30:29 -0500

    blog react node mongo

---
+++ BlogReactNodejs/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:'mongodb+srv://luisandia:h6QaX9PJPgpnYRr@cluster0-jgu16.mongodb.net/blog_dev?retryWrites=true',
+  cookieKey: '123123123'
+};
