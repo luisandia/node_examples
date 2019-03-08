d0173ef542e335fe43fb806b540af4d81dc94aa9
Andial <luisandiamamani@gmail.com>
2019-03-08 00:16:42 -0500

    hydrating cache

--- BlogReactNodejs/services/cache.js
+++ BlogReactNodejs/services/cache.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const redis = require('redis')
 const util = require('util')
-const redisUrl = 'redis://127.0.0.1:36590';
+const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
@@ -14,15 +14,20 @@ mongoose.Query.prototype.exec = async function () {
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
   }));
+  console.log("key");
   console.log(key);
 
   const cacheValue = await client.get(key);
-  // if(cacheValue){
-  //   console.log(cacheValue);
-  //   return JSON.parse(cacheValue);
-  // }
+  if (cacheValue) {
+    console.log("cache found");
+    console.log(cacheValue);
+    const doc = JSON.parse(cacheValue)
+    return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
+
+    // return data
+  }
   const result = await exec.apply(this, arguments);
 
-  client.set(key,JSON.stringify(result));
+  client.set(key, JSON.stringify(result));
   return result;
 }
