0eb8e3c47c68cf6bf9ed7164fa783c72e1b7988d
Andial <luisandiamamani@gmail.com>
2019-03-08 23:56:23 -0500

    cache middleware

---
+++ BlogReactNodejs/middlewares/cleanCache.js
@@ -0,0 +1,1 @@
+const { clearHash } = require('../services/cache');
--- BlogReactNodejs/services/cache.js
+++ BlogReactNodejs/services/cache.js
@@ -3,10 +3,20 @@ const redis = require('redis')
 const util = require('util')
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey =  JSON.stringify(options.key || '');
+  return this;
+}
+
 mongoose.Query.prototype.exec = async function () {
+  if(!this.useCache) {
+    return exec.apply(this,arguments);
+  }
+
   console.log('About to run a query');
   console.log(this.getQuery());
   console.log(this.mongooseCollection.name)
@@ -17,17 +27,21 @@ mongoose.Query.prototype.exec = async function () {
   console.log("key");
   console.log(key);
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey,key);
   if (cacheValue) {
     console.log("cache found");
     console.log(cacheValue);
     const doc = JSON.parse(cacheValue)
     return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
-
-    // return data
   }
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result),'EX',10);
   return result;
+}
+
+module.exports = {
+  clearHash(hashKey){
+    client.del(JSON.stringify(hashKey));
+  }
 }
