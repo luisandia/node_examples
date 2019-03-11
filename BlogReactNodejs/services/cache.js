dc088554102045db9d85bf7b666ed07550579d84
Andial <luisandiamamani@gmail.com>
2019-03-10 23:45:02 -0500

    add test oath flow

--- BlogReactNodejs/services/cache.js
+++ BlogReactNodejs/services/cache.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const redis = require('redis')
-const util = require('util')
+const redis = require('redis');
+const util = require('util');
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.hget = util.promisify(client.hget);
--- BlogReactNodejs/test/header.test.js
+++ BlogReactNodejs/test/header.test.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 let browser,page;
+
 beforeEach(async () => {
   browser = await puppeteer.launch({
     executablePath: '/usr/bin/google-chrome-stable',
@@ -8,11 +9,19 @@ beforeEach(async () => {
   page = await browser.newPage();
   await page.goto('localhost:3000');
 });
+
 afterEach(async ()=>{
   await browser.close();
 });
+
 test('We can launch a browser', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   console.log(text);
   expect(text).toEqual('Blogster');
 });
+
+test('clicking login starts oauth flow',async()=>{
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+})
