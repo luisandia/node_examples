aaea8675b56a6ea52c076dcbd0f9b8972b5158ac
Andial <luisandiamamani@gmail.com>
2019-03-16 19:23:53 -0500

    refactoring login test

--- BlogReactNodejs/test/helpers/page.js
+++ BlogReactNodejs/test/helpers/page.js
@@ -1,9 +1,12 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require('../factories/userFactory');
 
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: false,
+      executablePath: '/usr/bin/google-chrome-stable',
     });
 
     const page = await browser.newPage();
@@ -11,13 +14,26 @@ class CustomPage {
 
     return new Proxy(customPage, {
       get: function (target, property) {
-        return customPage[property] || page[property] || browser[property];
+        return customPage[property] || browser[property] || page[property];
       }
     });
   }
 
-  constructor() {
+  constructor(page) {
     this.page = page;
+  }
+
+  static close() {
+    this.browser.close();
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto('localhost:3000');
+    await this.page.waitFor('a[href="/auth/logout"]');
   }
 }
 
