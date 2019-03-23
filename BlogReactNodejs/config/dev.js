fbb6bcd03cc978ef2e629924991c3f64a2901c9c
Andial <luisandiamamani@gmail.com>
2019-03-22 23:55:00 -0500

    upload

--- BlogReactNodejs/config/dev.js
+++ BlogReactNodejs/config/dev.js
@@ -5,5 +5,7 @@ module.exports = {
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   mongoURI:'mongodb+srv://luisandia:h6QaX9PJPgpnYRr@cluster0-jgu16.mongodb.net/blog_dev?retryWrites=true',
   cookieKey: '123123123',
-  redisUrl:'redis://127.0.0.1:6379'
+  redisUrl:'redis://127.0.0.1:6379',
+  accessKeyId:'AKIAJKFHOR4GA5I6J4KA',
+  secretAccessKey:'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'
 };
